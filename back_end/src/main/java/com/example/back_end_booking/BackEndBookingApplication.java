c20b6e806a3cc0de9b209a34a596e4fab4344cb0
MSI\Admin <101080058+sydvph19885@users.noreply.github.com>
2024-04-05 11:06:46 +0700

    init backend

---
+++ back_end/src/main/java/com/example/back_end_booking/BackEndBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_end_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndBookingApplication.class, args);
+    }
+
+}
---
+++ back_end/src/test/java/com/example/back_end_booking/BackEndBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_end_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
